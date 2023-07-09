e86e31a44bf76658d0142713c38c0101d982c491
pwang96 <96pwang@gmail.com>
2023-07-09 15:14:53 -0400

    Add mui and add app header

--- ui/src/components/App.tsx
+++ ui/src/components/App.tsx
@@ -1,32 +1,17 @@
 import { useState } from 'react';
 
 import './App.css';
+import AppHeader from './AppHeader';
 import MessageBox from './MessageBox';
 
 const App = () => {
-  const [lastMessage, setLastMessage] = useState("hello world");
 
   return (
     <div className="App">
-      <header className="App-header">
+      <AppHeader />
+      <div>
         <MessageBox/>
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <div>
-          <p>
-            {lastMessage}
-          </p>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
